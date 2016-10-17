08e78bb94e936b68e2fe04eb7fac0725ce76f2f1
Will Lucas <mevatron@gmail.com>
2016-10-16 19:03:51 -0500

    Fixed bug with wget

--- install-idkit.sh
+++ install-idkit.sh
@@ -4,7 +4,7 @@ echo "Installing dependencies of idkit-2.72"
 sudo apt-get update && sudo apt-get install libusb-0.1-4 libtbb2 libopencv-dev wget
 
 echo "Installing idkit-2.72"
-wget -qO- /tmp/idkit_2.72ubuntu1_amd64.deb https://github.com/DFTinc/onyx-node-rest/releases/download/2.72/idkit_2.72ubuntu1_amd64.deb
+wget -qO /tmp/idkit_2.72ubuntu1_amd64.deb https://github.com/DFTinc/onyx-node-rest/releases/download/2.72/idkit_2.72ubuntu1_amd64.deb
 
 sudo dpkg -i /tmp/idkit_2.72ubuntu1_amd64.deb
 
